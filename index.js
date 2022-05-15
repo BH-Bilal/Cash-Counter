8f891f7365aa6299bb1aa5db1ffffb524d9eecc5
Bilal-9 <bilalhussain1115@gmail.com>
2022-05-15 08:44:52 -0400

    updates

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const bill = document.querySelector("#bill-Amount");
 const cashgiven = document.querySelector("#cash-given");
 const checkbutton = document.querySelector("#check-button");
 const msg = document.querySelector("#error-message")
-checkbutton.addEventListener("click", function validateBillAndCashAmount ()
+
 {
     message.style.display = "none";
 if (bill.value > 0 ){
@@ -11,9 +11,10 @@ if (bill.value > 0 ){
 } else {
 
 }
-});
+}
 function showMessage (message){
     message.style.display = "Block";
     message.innerText = "The Bill amount should be grater than 0";
 
 }
+checkbutton.addEventListener("click", function validateBillAndCashAmount ());
