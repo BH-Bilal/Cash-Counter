0894a0f6a4747be26b1a33178f564836f2fa7ba7
Bilal-9 <bilalhussain1115@gmail.com>
2022-06-01 11:36:33 +0530

    updates

--- index.js
+++ index.js
@@ -1,20 +1,49 @@
-const bill = document.querySelector("#bill-Amount");
-const cashgiven = document.querySelector("#cash-given");
-const checkbutton = document.querySelector("#check-button");
-const msg = document.querySelector("#error-message")
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given");
+const checkButton = document.querySelector("#check-button");
+const message = document.querySelector("#error-message");
+const noOfNotes = document.querySelectorAll(".no-of-notes");
 
-{
+const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
+
+checkButton.addEventListener("click", function validateBillAndCashAmount() {
+    hideMessage();
+    if (billAmount.value > 0) {
+        // 12
+        if (cashGiven.value >= billAmount.value) {
+            // 2022> 12 => true
+            const amountToBeReturned = cashGiven.value - billAmount.value; // 2022 - 12 = 2010
+            calculateChange(amountToBeReturned);
+        } else {
+            showMessage("Do you wanna wash plates?");
+        }
+    } else {
+        showMessage("Invalid Bill Amount");
+    }
+});
+
+function calculateChange(amountToBeReturned) {
+    // 2010
+    // go over all the available
+    for (let i = 0; i < availableNotes.length; i++) {
+        // no of notes need for the denomination
+        const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
+        // 2010 / 2000 = 1 || 10 / 500 = 0
+
+        // amount left after calculating the number of notes needed
+        amountToBeReturned = amountToBeReturned % availableNotes[i];
+        // 2010 % 2000 = 10 || 10 % 500 = 10
+
+        // updating the no of notes in the table for the current amount
+        noOfNotes[i].innerText = numberOfNotes;
+    }
+}
+
+function hideMessage() {
     message.style.display = "none";
-if (bill.value > 0 ){
-    if (cashgiven.value >= bill.value){}
-
-} else {
-
 }
-}
-function showMessage (message){
-    message.style.display = "Block";
-    message.innerText = "The Bill amount should be grater than 0";
 
+function showMessage(msg) {
+    message.style.display = "block";
+    message.innerText = msg;
 }
-checkbutton.addEventListener("click", function validateBillAndCashAmount ());
